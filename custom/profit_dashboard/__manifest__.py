e50aa3790fec55ce51b3edae97dbfff5150a4493
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 13:09:27 -0600

    Refactor profit_dashboard manifest for assets
    Removed manifest structure and retained asset definitions.

--- custom/profit_dashboard/__manifest__.py
+++ custom/profit_dashboard/__manifest__.py
@@ -1,16 +1,5 @@
-{
-    "name": "Profit Dashboard",
-    "version": "1.0",
-    "depends": ["web"],
-    "data": [
-        "views/dashboard.xml",
-    ],
-    "assets": {
-        "web.assets_backend": [
-            "profit_dashboard/static/lib/chart.js",
-            "profit_dashboard/static/src/js/profit_chart.js",
-            "profit_dashboard/static/src/xml/profit_chart.xml",
-        ],
-    },
-    "installable": True,
-}
+"web.assets_backend": [
+    "profit_dashboard/static/lib/chart.js",
+    "profit_dashboard/static/src/js/profit_chart.js",
+    "profit_dashboard/static/src/xml/profit_chart.xml",
+],
