2be59ad647b2a23906e4e609a002113b37ef62b5
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 13:07:02 -0600

    Add chart rendering in ProfitChart component

--- custom/profit_dashboard/static/src/js/profit_chart.js
+++ custom/profit_dashboard/static/src/js/profit_chart.js
@@ -1,9 +1,32 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from "@odoo/owl";
+import { Component, onMounted } from "@odoo/owl";
 
-export class ProfitChart extends Component {}
+export class ProfitChart extends Component {
+    setup() {
+        onMounted(() => {
+            if (!window.Chart) {
+                console.error("Chart.js not loaded");
+                return;
+            }
+
+            const ctx = document.getElementById("profitChart");
+
+            new window.Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: ["Test"],
+                    datasets: [{
+                        label: "Ventas",
+                        data: [55],
+                        backgroundColor: "#d32f2f",
+                    }]
+                }
+            });
+        });
+    }
+}
 
 ProfitChart.template = "profit_dashboard.ProfitChart";
 
