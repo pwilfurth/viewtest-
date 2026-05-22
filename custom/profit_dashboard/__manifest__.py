b00b43e9c5a74148a269e3031eacf2dc54f79e74
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 18:23:39 -0600

    Remove web.assets_qweb from manifest
    Removed QWeb assets from the manifest file.

--- custom/profit_dashboard/__manifest__.py
+++ custom/profit_dashboard/__manifest__.py
@@ -2,7 +2,4 @@
     "web.assets_backend": [
         "profit_dashboard/static/src/js/profit_chart.js",
     ],
-    "web.assets_qweb": [
-        "profit_dashboard/static/src/xml/profit_chart.xml",
-    ],
 },
