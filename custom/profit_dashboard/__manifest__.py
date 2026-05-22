69d0d3b411888d7ffb534f32b34345a3650b25a2
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 18:22:11 -0600

    Add profit_chart.js to backend assets

--- custom/profit_dashboard/__manifest__.py
+++ custom/profit_dashboard/__manifest__.py
@@ -1,6 +1,8 @@
 "assets": {
     "web.assets_backend": [
         "profit_dashboard/static/src/js/profit_chart.js",
+    ],
+    "web.assets_qweb": [
         "profit_dashboard/static/src/xml/profit_chart.xml",
     ],
 },
