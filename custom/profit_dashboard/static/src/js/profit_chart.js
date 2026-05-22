8bc51d2bed8dc8042a5a390830dc6f2988e3d0cd
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 18:12:53 -0600

    Refactor ProfitChart to use start method

--- custom/profit_dashboard/static/src/js/profit_chart.js
+++ custom/profit_dashboard/static/src/js/profit_chart.js
@@ -1,12 +1,9 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from "@odoo/owl";
 
-export class ProfitChart extends Component {}
-ProfitChart.template = "profit_dashboard.ProfitChart";
-
-registry.category("actions").add(
-    "profit_dashboard.ProfitChart",
-    ProfitChart
-);
+registry.category("actions").add("profit_dashboard.ProfitChart", {
+    start() {
+        document.body.innerHTML += "<h1>Profit Dashboard Loaded</h1>";
+    },
+});
