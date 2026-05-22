b319a4748e5a10b1629261f436a71842974d65e4
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 18:10:42 -0600

    Modify manifest to add assets for profit dashboard
    Updated manifest to include asset definitions for JavaScript and XML files.

--- custom/profit_dashboard/__manifest__.py
+++ custom/profit_dashboard/__manifest__.py
@@ -1,10 +1,6 @@
-{
-    "name": "Profit Dashboard",
-    "version": "1.0",
-    "depends": ["web"],
-    "data": [
-        "views/dashboard.xml",
+"assets": {
+    "web.assets_backend": [
+        "profit_dashboard/static/src/js/profit_chart.js",
+        "profit_dashboard/static/src/xml/profit_chart.xml",
     ],
-    "installable": True,
-    "application": True,
-}
+},
