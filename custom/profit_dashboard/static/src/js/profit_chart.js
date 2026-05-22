86e3d00ce1f69b60f5cb11ee9a2f8dd3fd4579fb
pwilfurth <60961015+pwilfurth@users.noreply.github.com>
2026-05-21 18:23:16 -0600

    Add template to ProfitChart component

--- custom/profit_dashboard/static/src/js/profit_chart.js
+++ custom/profit_dashboard/static/src/js/profit_chart.js
@@ -1,11 +1,15 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from "@odoo/owl";
+import { Component, xml } from "@odoo/owl";
 
-class ProfitChart extends Component {
-    static template = "profit_dashboard.ProfitChart";
-}
+class ProfitChart extends Component {}
+
+ProfitChart.template = xml`
+    <div class="o_profit_dashboard">
+        <h1>Profit Dashboard</h1>
+    </div>
+`;
 
 registry.category("actions").add(
     "profit_dashboard.ProfitChart",
